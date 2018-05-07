ab03b5b7c023b89470553be9347c41af0956a6ba
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-07 16:45:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,12 @@ def countdown(seconds)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
+end
